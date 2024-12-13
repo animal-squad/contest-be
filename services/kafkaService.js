a9339ea5e1e4c733b75af324b30cdb6267cedb7c
미나 이 <chsjen492@gmail.com>
2024-12-13 18:24:19 +0900

    fix: modify broker setting

--- services/kafkaService.js
+++ services/kafkaService.js
@@ -5,7 +5,7 @@ class KafkaService {
     constructor() {
         this.kafka = new Kafka({
             clientId: 'chat-service',
-            brokers: process.env.KAFKA_BROKERS
+            brokers: process.env.KAFKA_BROKERS?.split(',')
         });
 
         this.producer = this.kafka.producer({
