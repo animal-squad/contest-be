4fd52b2535692bae2de7028b945deba9d7c56df7
미나 이 <chsjen492@gmail.com>
2024-12-13 17:48:32 +0900

    fix: remove broker localhost

--- services/kafkaService.js
+++ services/kafkaService.js
@@ -5,7 +5,7 @@ class KafkaService {
     constructor() {
         this.kafka = new Kafka({
             clientId: 'chat-service',
-            brokers: process.env.KAFKA_BROKERS?.split(',') || ['localhost:9092']
+            brokers: process.env.KAFKA_BROKERS?.split(',')
         });
 
         this.producer = this.kafka.producer({
